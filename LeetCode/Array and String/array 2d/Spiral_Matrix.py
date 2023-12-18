92a6d2976b42fca210b9d875020787bb09f52d64
Роман <Sudorenkoroma@gmail.com>
2023-12-18 12:51:12 +0200

    adds N1 18.12.2023

--- LeetCode/Array and String/array 2d/Spiral_Matrix.py
+++ LeetCode/Array and String/array 2d/Spiral_Matrix.py
@@ -23,8 +23,8 @@ class Solution:
         return result
 
 
-mat = [[1,2,3],[4,5,6],[7,8,9]]
+mat = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+
 res = Solution()
 sol = res.spiralOrder(mat)
-
 print(sol)
